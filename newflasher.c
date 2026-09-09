a49e09d770e1c461c111227102f4023d0a85e0c7
Wehrwolfmann <256216494+wehrwolfmann@users.noreply.github.com>
2026-09-09 23:09:12 +0200

    Tell what went wrong when trim area partition returns no data

--- newflasher.c
+++ newflasher.c
@@ -3311,7 +3311,7 @@ int main(int argc, char *argv[])
 
 							if (!data_len)
 							{
-								printf("got null data_len!\n");
+								printf("Device returned no data for trimarea partition %d! Dump is incomplete, retry it after replugging the phone into flashmode.\n", i);
 
 								if (!get_reply(dev, EP_IN, tmp, sizeof(tmp), USB_TIMEOUT, 0))
 								{
